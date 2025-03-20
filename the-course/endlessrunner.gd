c94cacc17dc02c84a98338d8bba6dec85e2ccdc2
AmazenYT <frankiecahill90@gmail.com>
2025-03-20 20:13:28 +0000

    platform scene

---
+++ the-course/endlessrunner.gd
@@ -0,0 +1,13 @@
+extends CharacterBody3D
+
+@export var speed: float = 10.0
+@export var gravity: float = -24.8
+@export var jump_force: float = 10.0
+
+
+func _physics_process(delta: float) -> void:
+	velocity.y += gravity * delta
+	if is_on_floor() and Input.is_action_just_pressed("ui_select"):
+		velocity.y = jump_force
+	velocity.z = speed
+	move_and_slide()
