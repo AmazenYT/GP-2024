ee4674b332a090d5c10914fc59509b8aed042107
AmazenYT <frankiecahill90@gmail.com>
2025-03-20 20:24:03 +0000

    coin scene

--- the-course/platform_spawner.gd
+++ the-course/platform_spawner.gd
@@ -5,16 +5,16 @@ extends Node3D
 @export var platform_speed: float = 10.0
 
 func _ready() -> void:
-    spawn_platform()
+	spawn_platform()
 
 func spawn_platform() -> void:
-    var platform = platform_scene.instantiate()
-    add_child(platform)
-    platform.position.z = spawn_distance
+	var platform = platform_scene.instantiate()
+	add_child(platform)
+	platform.position.z = spawn_distance
 
 func _process(delta: float) -> void:
-    for platform in get_children():
-        platform.position.z -= platform_speed * delta
-        if platform.position.z < -10:
-            platform.queue_free()
-            spawn_platform()
+	for platform in get_children():
+		platform.position.z -= platform_speed * delta
+		if platform.position.z < -10:
+			platform.queue_free()
+			spawn_platform()
